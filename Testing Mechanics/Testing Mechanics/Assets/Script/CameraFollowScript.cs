a8b4d23de5916bc368b18161451b2df9ddb915db
DesmondYeoh-LeineX <desmondyeoh22@gmail.com>
2021-08-27 15:35:48 +0800

    Parallax Camera

---
+++ Testing Mechanics/Testing Mechanics/Assets/Script/CameraFollowScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public GameObject playerRef;
+    public float heightDifference;
+
+    private Vector2 selfPos;
+    private Vector2 playerPosRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selfPos = new Vector2 (transform.position.x, transform.position.y);
+        playerPosRef = new Vector2 (playerRef.transform.position.x, playerRef.transform.position.y + heightDifference);
+        if(selfPos != playerPosRef)
+        {
+            selfPos = playerPosRef;
+            transform.position = new Vector3(selfPos.x, selfPos.y, transform.position.z);
+        }
+    }
+}
