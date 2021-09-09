04d1d201e53c4ca6659c86d628ca90130a4ddcfa
DesmondYeoh-LeineX <desmondyeoh22@gmail.com>
2021-09-10 06:11:08 +0800

    Correct Version

---
+++ Testing Mechanics/Testing Mechanics/Assets/Script/PreloadScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PreloadScript : MonoBehaviour
+{
+    public Image poster;
+    public float fadeRate;
+    public float preloadTime;
+
+    private float alphaLevel;
+    private float maxAlpha = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alphaLevel = 0.0f;
+        StartCoroutine("NextScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(alphaLevel < maxAlpha)
+        {
+            alphaLevel += fadeRate * Time.deltaTime;
+            poster.color = new Color(255, 255, 255, alphaLevel);
+        }
+    }
+
+    IEnumerator NextScene()
+    {
+        yield return new WaitForSeconds(preloadTime);
+        SceneManager.LoadScene("Menu");
+    }
+}
